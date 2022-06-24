024fd010e422524dc462541fc728fd91e4bd8a12
GOPIKRISHNA44 <bhargav.gandham44@gmail.com>
2022-06-24 17:44:32 -0500

    Changes

--- Server/code/src/main/java/com/quorastudent/controllers/UserController.java
+++ Server/code/src/main/java/com/quorastudent/controllers/UserController.java
@@ -1,8 +1,38 @@
 package com.quorastudent.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.quorastudent.dto.ResponseDTO;
+
 @RestController
+@RequestMapping(value = "user")
 public class UserController {
+
+	@RequestMapping(value = "register", method = RequestMethod.POST)
+	@ResponseBody
+	public ResponseDTO register() {
+		return null;
+	}
+
+	@RequestMapping(value = "login", method = RequestMethod.POST)
+	@ResponseBody
+	public ResponseDTO login() {
+		return null;
+	}
+
+	@RequestMapping(value = "checksession", method = RequestMethod.POST)
+	@ResponseBody
+	public ResponseDTO checkSession() {
+		return null;
+	}
+
+	@RequestMapping(value = "logout", method = RequestMethod.POST)
+	@ResponseBody
+	public ResponseDTO logout() {
+		return null;
+	}
 
 }
---
+++ Server/code/src/main/java/com/quorastudent/services/UserService.java
@@ -0,0 +1,24 @@
+package com.quorastudent.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+	public Object register() {
+		return null;
+	}
+
+	public Object login() {
+		return null;
+	}
+
+	public Object checkSession() {
+		return null;
+	}
+
+	public Object logout() {
+		return null;
+	}
+
+}
