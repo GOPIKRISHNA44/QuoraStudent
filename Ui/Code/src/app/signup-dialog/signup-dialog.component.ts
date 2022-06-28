5e6f5925f6e33a191c9f800925825f1143a9d191
Anvesh-Karnati <anveshnani40@gmail.com>
2022-06-28 13:41:13 -0500

    login changes

---
+++ Ui/Code/src/app/signup-dialog/signup-dialog.component.ts
@@ -0,0 +1,21 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import {  MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+@Component({
+  selector: 'app-signup-dialog',
+  templateUrl: './signup-dialog.component.html',
+  styleUrls: ['./signup-dialog.component.css']
+})
+export class SignupDialogComponent implements OnInit {
+
+  constructor( public dialogRef: MatDialogRef<SignupDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  ngOnInit(): void {
+  }
+  onNoClick(){
+    this.dialogRef.close();
+  }
+  signUp(){
+    this.dialogRef.close();
+  }
+}
